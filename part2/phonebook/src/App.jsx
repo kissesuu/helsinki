83c66fa80a6ba60bf52fc88f1fd808842c77311d
Yasmin Almario <yasmin.almario@velaindustriesgroup.com>
2025-06-20 15:25:48 +0800

    Commit for part 2.6, added events

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat(newName))
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p key={person}>{person}</p>)}
     </div>
   )
 }
