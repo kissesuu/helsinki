ac114f9a20132a9f350ecf22d89afb1639f33f42
Yasmin Almario <yasmin.almario@velaindustriesgroup.com>
2025-06-20 13:34:38 +0800

    Commit part 2.5, separate module

--- part2/courses/src/App.jsx
+++ part2/courses/src/App.jsx
@@ -1,94 +1,10 @@
-const Header = (props) => {
-  return <h2>{props.course}</h2>
-}
-
-const Total = (props) => {
-  const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <b>
-      total of{' '}
-      {totalExercises}
-      {' '}exercises
-    </b>
-  );
-}
-
-const Content = (props) => {
-  const parts = props.parts.map(part => <Part key={part.name} part={part} />);
-  return (
-      <div>
-        {parts}
-      </div>
-  );
-}
-
-const Part = (props) => {
-  return <p>{props.part.name} {props.part.exercises}</p>;
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts}/>
-    </div>
-  )
-}
+import Courses from './components/Courses';
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-  const courseLine = courses.map(course => <Course key={course.id} course={course} />);
-
   return (
     <div>
       <h1>Web development curriculum</h1>
-      {courseLine}
+      {Courses}
     </div>
   );
 }
